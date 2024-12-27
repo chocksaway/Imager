3b8767416e34b91e5ba45f7b05cc57cbc48b0b64
Miles Davenport <milesd@chocksaway.com>
2024-12-27 12:33:26 +0000

    Fixed broken test.

--- src/test/java/repository/LoginRepositoryTests.java
+++ src/test/java/repository/LoginRepositoryTests.java
@@ -1,10 +1,12 @@
 package repository;
 
+import com.chocksaway.imager.ImagerApplication;
 import com.chocksaway.imager.domain.Login;
 import com.chocksaway.imager.repository.LoginRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,10 +14,18 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
-class LoginRepositoryTests {
+@SpringBootTest(classes = ImagerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LoginRepositoryTests {
+
+    @LocalServerPort
+    private int port;
+
+    private final LoginRepository loginRepository;
+
     @Autowired
-    private LoginRepository loginRepository;
+    public LoginRepositoryTests(LoginRepository loginRepository) {
+        this.loginRepository = loginRepository;
+    }
 
     @Test
     public void given_a_login_return_the_password() {
@@ -23,13 +33,12 @@ class LoginRepositoryTests {
         loginRepository.save(login);
 
         List<Login> loginList = loginRepository.findLoginByName("milesd");
-        Optional<String> foundLogin= loginList.stream()
+        Optional<String> foundLogin = loginList.stream()
                 .filter(each -> each.getName().equals("milesd"))
-                        .findFirst()
-                        .map(Login::getPassword);
+                .findFirst()
+                .map(Login::getPassword);
 
         assertTrue(foundLogin.isPresent());
-
         assertEquals("password", foundLogin.get());
     }
 }
